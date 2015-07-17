76e8d0756827e3c7a6d1884687e79e8a7f9e1dae
Jerry Jiang <jerry@zhjdeMacBook-Pro.local>
2015-07-17 13:21:46 +0800

    Initial Commit

---
+++ JobManager/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JobManager
+//
+//  Created by Jerry Jiang on 17/7/15.
+//  Copyright (c) 2015 Jerry Jiang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
